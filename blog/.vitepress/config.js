2dd85ebddc0d14446c92eabce3166ea2f7f5b9ab
yujingaya <yujingaya@linecorp.com>
2025-10-08 14:22:15 +0900

    Add Google Analytics

--- blog/.vitepress/config.js
+++ blog/.vitepress/config.js
@@ -8,5 +8,20 @@ export default defineConfig({
     appearance: 'force-auto',
     sitemap: {
         hostname: 'https://nest.yujingaya.com'
-    }
+    },
+    head: [
+        [
+            'script',
+            { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-7EDSB12GEY' }
+        ],
+        [
+            'script',
+            {},
+            `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-7EDSB12GEY');`
+        ]
+    ]
 })
